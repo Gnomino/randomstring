169f628dd964dc88418fd4d8c93bf0f6cd58dfc4
gnomino <gnomino@gDeb64>
2014-06-07 12:30:03 +0200

    Now a python3 script

--- randomstring.py
+++ randomstring.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import string
 from random import choice
 import argparse
 
 parser = argparse.ArgumentParser(description='Generates random strings')
 parser.add_argument('length', metavar='length', type=int, help='The length of generated string')
-parser.add_argument('-l', '--lowers', action='store_const', dest='lowers', const=string.lowercase, default='', help='Adds lower cases to the random string')
-parser.add_argument('-u', '--uppers', action='store_const', dest='uppers', const=string.uppercase, default='', help='Adds upper cases to the random string')
+parser.add_argument('-l', '--lowers', action='store_const', dest='lowers', const=string.ascii_lowercase, default='', help='Adds lower cases to the random string')
+parser.add_argument('-u', '--uppers', action='store_const', dest='uppers', const=string.ascii_uppercase, default='', help='Adds upper cases to the random string')
 parser.add_argument('-s', '--symbols', action='store_const', dest='symbols', const=string.punctuation, default='', help='Adds symbols to the random string')
 parser.add_argument('-w', '--whitespaces', action='store_const', dest='whitespaces', const=string.whitespace, default='', help='Adds whitespaces to the random string')
 parser.add_argument('-n', '--numbers', action='store_const', dest='numbers', const=string.digits, default='', help='Adds numbers to the random string')
@@ -19,4 +19,4 @@ if args.all == '':
 else:
   chars = args.all
 
-print "".join(choice(chars) for i in range(args.length))
+print ("".join(choice(chars) for i in range(args.length)))
